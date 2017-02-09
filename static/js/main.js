8839cc5a42200546f8b97eb0f7e022e95de6a112
Qais Patankar <qaisjp@gmail.com>
2017-02-09 10:45:09 +0000

    only pass mouse events to map after clicking in

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$('.parallax').parallax();
+
+	$('#location-map')
+		.mousedown(
+			function(){
+				$(this).find('iframe').addClass('clicked')
+			}
+		)
+		.mouseleave(
+			function(){
+				$(this).find('iframe').removeClass('clicked')
+			}
+		);
 })
