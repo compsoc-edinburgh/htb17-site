165f34527f1e803afca4b162314ef3d4dcf4bf09
Qais Patankar <qaisjp@gmail.com>
2017-03-18 10:12:55 +0000

    make it pretty for merge

--- static/js/live.js
+++ static/js/live.js
@@ -104,7 +104,10 @@ $(document).ready(function(){
     checkMouseMove();
 
     var clock = $('#countdown');
-    updateDeadline('March 18 2017 11:00:00 GMT+0000');
+    updateDeadline('March 18 2017 11:00:00 GMT+0000', "until opening ceremony!");
+    // updateDeadline('March 18 2017 12:00:00 GMT+0000', "until hacking begins!");
+    // updateDeadline('March 19 2017 17:00:00 GMT+0000', "until hacking ends!");
+
     updateClock(clock); // run function once at first to avoid delay
     var timeinterval = setInterval(updateClock,1000, clock)
 });
